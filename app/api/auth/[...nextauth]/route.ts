977b4990d5b8ea168db9531db252bd22a0d287e7
MiNeoSatory <stanislav.limanovski@gmail.com>
2024-06-15 11:49:08 +0300

    Imports are edited

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/app/libs/auth';
+import authOptions from '@/app/libs/auth';
 
 const handler = NextAuth(authOptions);
 
--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -7,7 +7,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 import prisma from '@/app/libs/prismadb';
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
@@ -55,3 +55,5 @@ export const authOptions: AuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
+
+export default authOptions;
